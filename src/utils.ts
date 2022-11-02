a3af70785a01451194f6c8dd8ee476fc87fb27d4
saifaldin14 <saifaldindeveloper@gmail.com>
2022-11-02 15:55:14 -0400

    Create utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+export function* range(start: number, stop: number, step = 1) {
+  if (stop == null) {
+    // one param defined
+    stop = start;
+    start = 0;
+  }
+
+  for (let i = start; step > 0 ? i < stop : i > stop; i += step) {
+    yield i;
+  }
+}
+
+export function getPermutations(array: string | any[], size: any) {
+  function p(t: string | any[], i: number) {
+    if (t.length === size) {
+      result.push(t);
+      return;
+    }
+    if (i + 1 > array.length) {
+      return;
+    }
+    p(t.concat(array[i]), i + 1);
+    p(t, i + 1);
+  }
+
+  const result: any[] = [];
+  p([], 0);
+  return result;
+}
