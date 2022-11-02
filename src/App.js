daad99ef7ca94d06e69668f45c47c4db34eaa111
saifaldin14 <saifaldindeveloper@gmail.com>
2022-11-02 13:52:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import SudokuSolver from "./components/SudokuSolver";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="text-center">
+      <h1 className="mt-3">Sudoku Solver</h1>
+      <hr />
+      <SudokuSolver />
+    </Container>
   );
 }
 
