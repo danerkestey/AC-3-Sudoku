b7687320e4842ac6c7079fdc4f032db2fe07b77e
Chandler Mayberry <chandlermayberry@live.ca>
2022-11-05 17:56:32 -0400

    Added the default games

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ layout = [
     [gui.Text('G', size=(0, 0)), gui.InputText()],
     [gui.Text('H', size=(0, 0)), gui.InputText()],
     [gui.Text('I ', size=(0, 0)), gui.InputText()],
-    [gui.Submit(), gui.Button('Exit', button_color=('white', 'grey')), gui.Button('DefaultGame', button_color=('white', 'teal'))]]
+    [gui.Submit(), gui.Button('Exit', button_color=('white', 'grey')), gui.Button('DefaultGame', button_color=('white', 'teal')), gui.Button('DefaultBackTrackingGame', button_color=('white', 'pink'))]]
 
 
 window = gui.Window('--Sudoku Puzzle Solver--',
@@ -40,6 +40,32 @@ while True:
 
     if event in (None, 'Exit'):
         break
+    
+    elif event in (None, 'DefaultAC3Game'):
+        
+        #run the AC3 solvable:
+        defaultGame1 = [['003020600'],
+                       ['900305001'],
+                       ['001806400'],
+                       ['008102900'],
+                       ['700000008'],
+                       ['006708200'],
+                       ['002609500'],
+                       ['800203009'],
+                       ['005010300']]
+    
+    elif event in (None, 'DefaultBackTrackingGame'):
+        #run the AC3 unsolvable for backtrack testing:
+        defaultGame1 = [['000100702'],
+                       ['030950000'],
+                       ['001002003'],
+                       ['590000301'],
+                       ['020000070'],
+                       ['703000098'],
+                       ['800200100'],
+                       ['000085060'],
+                       ['605009000']]
+    
     else:
         puzzle = buildPuzzle(values)
         print(puzzle)
