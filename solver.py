486a9b0ac760ddfc79c1bd48682336527b6d72ee
saifaldin14 <saifaldindeveloper@gmail.com>
2022-11-06 16:19:20 -0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -3,7 +3,17 @@
 class SudokuPuzzleBox:
     def __init__(self, puzzleBox):
         self.puzzleBox = puzzleBox
-    
+
+    def turnTo2DList(puzzle):
+        # Accepts a List of strings
+        ret = []
+        for lvl in puzzle:
+            temp = []
+            for c in lvl:
+                temp.append(int(c))
+            ret.append(temp)
+        return ret
+
     def printBox(self):
         num = 0
         for i in self.puzzleBox:
@@ -12,7 +22,8 @@ class SudokuPuzzleBox:
             else:
                 print()
                 num = 0
-            num+=1
+            num += 1
+
 
 class SudokuSolver:
     def __init__(self, poggers):
