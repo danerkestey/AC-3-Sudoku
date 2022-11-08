1b1fe173734700791d254c1acacbb539541a459e
saifaldin14 <saifaldindeveloper@gmail.com>
2022-11-08 15:40:02 -0500

    Update solver.py

--- src/gui/function/solver.py
+++ src/gui/function/solver.py
@@ -32,53 +32,82 @@ def getHintBoard(gameBoard, originalBoard, hintBoard):
     return hintBoard.copy()
 
 
-def solve(bo):
+"""
+Definition: Function to solve the Sudoku problem using backtracking
+    Input:
+        board (list) - Current arc-consistent instance of the Sudoku puzzle
+    Returns:
+        Whether or not the puzzle was solved
+"""
+
+
+def solve(board):
     # Solves by backtracking
-    find = find_empty(bo)
+    find = find_empty(board)
     if not find:
         return True
     else:
         row, col = find
 
     for i in range(1, 10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        if valid(board, i, (row, col)):
+            board[row][col] = i
 
-            if solve(bo):
+            if solve(board):
                 return True
 
-            bo[row][col] = 0
+            board[row][col] = 0
 
     return False
 
 
-def valid(bo, num, pos):
-    # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+"""
+Definition: Function to find the first empty position
+    Input:
+        board (list) - Current arc-consistent instance of the Sudoku puzzle
+        num (int) - Current value in the board
+        pos (list) - Tuple representing the position of the number
+    Returns:
+        (i, j) - Position of the first empty cell
+"""
+
+
+def valid(board, num, pos):
+    # Check Row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
             return False
 
-    # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    # Check Column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
             return False
 
-    # Check box
+    # Check Square
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i, j) != pos:
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
                 return False
 
     return True
 
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+"""
+Definition: Function to find the first empty position
+    Input:
+        board (list) - Current arc-consistent instance of the Sudoku puzzle
+    Returns:
+        (i, j) - Position of the first empty cell
+"""
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
                 return (i, j)  # row, col
 
     return None
